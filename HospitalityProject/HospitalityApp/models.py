7b2e2a5d008eb46df1e608bb9a647bd8f74b876c
Thorin, the Bearded <johnjonsteyn@tutanota.com>
2023-06-03 14:03:04 +0200

    Add Patient Table
    + Add Patient Table

--- HospitalityProject/HospitalityApp/models.py
+++ HospitalityProject/HospitalityApp/models.py
@@ -8,3 +8,15 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+class Patient(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    contact_number = models.CharField(max_length=20)
+    address = models.TextField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
