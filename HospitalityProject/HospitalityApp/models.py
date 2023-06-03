e05a38491c53c6fe761002203ee40c01fc32b39a
Thorin, the Bearded <johnjonsteyn@tutanota.com>
2023-06-03 14:35:47 +0200

    Add Equipment Table
    + Add equipment table.
    + Migrate.
    + Update roadmap.

--- HospitalityProject/HospitalityApp/models.py
+++ HospitalityProject/HospitalityApp/models.py
@@ -64,3 +64,13 @@ class Prescription(models.Model):
 
     def __str__(self):
         return f'Prescription for {self.patient}'
+
+class Equipment(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    quantity = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
